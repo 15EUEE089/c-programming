29d4efe62220de859f347c499e59a8e6f4b98e47
15EUEE089 <34646529+15EUEE089@users.noreply.github.com>
2017-12-19 14:16:56 +0530

    Create variable.c

---
+++ beginner/variable.c
@@ -0,0 +1,11 @@
+#include<iostream.h>
+int main(void)
+{
+char name[30]="Sachin Ramesh Tendulkar";
+char nickname[30]="Little Master";
+int runs=18426;
+float avg=44.83;
+int matches=463;
+printf("%s\n%s\n%d\n%f\n%d\n",name,nickname,runs,avg,matches);
+return 0;
+}
